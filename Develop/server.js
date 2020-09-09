9ec30cca94e79f115a05ffc83c03575c41dfc636
TJ <tahmeenaowais@yahoo.com>
2020-09-09 09:58:22 -0400

    sysnc data

--- Develop/server.js
+++ Develop/server.js
@@ -7,64 +7,61 @@ var PORT = 3060;
 //const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
-const note = [
-   {
+const note = [{
   title: "",
   text: "",
 
- 
-  },
-]
-var id= note.title
-  app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  
-  });
- 
-  app.delete("/notes/"+id, function(req, res) {
-    res.delete
-  
-  });
-app.get("/api/notes", function(req, res) {
-    fs.readFile("db/db.json", function (err, data) {
-         if (err) {
-            return console.log(err);
-        }
-    
-        console.log("Success!", JSON.parse(data));
 
-        res.json(JSON.parse(data))
-    
-   
-    })
+}, ]
+var id = note.title
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+
 });
 
-app.post("/api/notes", function(req, res) {
+app.delete("/notes/" + id, function (req, res) {
+  res.delete
+
+});
+app.get("/api/notes", function (req, res) {
+  fs.readFile("db/db.json", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success!", JSON.parse(data));
+
+    res.json(JSON.parse(data))
+
+
+  })
+});
+
+app.post("/api/notes", function (req, res) {
   var newNote = req.body;
-  note.push([newNote]);
-    res.json([newNote])
-  fs.writeFile("db/db.json",JSON.stringify([newNote]) ,function(err){
-      if (err) {
-          return console.log(err);
-      }
-  
-      console.log("Success!", ([newNote]))
+  note.push(newNote);
+  res.json(newNote)
+  fs.writeFileSync("db/db.json", JSON.stringify([newNote]), function (err) {
+    if (err) {
+      return console.log(err);
+    }
 
- })
+    console.log("Success!")
+
+  })
 });
-   
-          
-
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-
-    });
-app.listen(PORT, function() {
-    console.log("App listening on localhost: " + PORT);
-  });
 
 
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+
+});
+app.listen(PORT, function () {
+  console.log("App listening on localhost: " + PORT);
+});
